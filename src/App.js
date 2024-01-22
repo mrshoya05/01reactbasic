f0b0e7b3b32fa251cfb0605bc128701dcecf958b
shoyab <shoyabkhan0508@gmail.com>
2024-01-22 10:35:14 -0800

    basic ract concepts

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <>
+    <h2>rander this file </h2>
+    </>
+  );
+}
+
+export default App;
---
+++ src/chai.js
@@ -0,0 +1,8 @@
+function Shoya(){
+    return(
+        <>
+        <h3>hello world shoya!</h3>
+        </>
+    )
+}
+export default Shoya
